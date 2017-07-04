cafd583efc36fe0632c9706b715c9108340e84a4
Nadezhda Sergeeva <sergeeva@morizo.ru>
2017-07-04 22:35:18 +0300

    Обновление проекта старт

--- src/js/components/cmn-back.js
+++
@@ -1,6 +0,0 @@
-export default function () {
-
-	$('.js-cmn-back').on('click', function(){
-		history.go(-1);
-	});
-}
---
+++ src/js/components/trans-table.js
@@ -0,0 +1,5 @@
+export default function () {
+
+    $('table').not('.no-rotate').transTable();
+
+}
