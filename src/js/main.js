3481ff74ec610c1d716903534a22f9102309c091
Nadezhda Sergeeva <sergeeva@morizo.ru>
2018-05-06 21:59:21 +0300

    Чистим скрипты

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import sliders from './components/sliders';
 import gumburger from './components/gumburger';
 import popups from './components/popups';
 import validate from './components/validate';
-import subscribe from './components/subscribe';
 import accordion from './components/accordion';
 import textTable from './components/text-table';
 import fullscreenMenu from './components/fullscreen-menu';
@@ -11,12 +10,10 @@ import flexMenu from './components/flex-menu';
 import tabsComp from './components/tabs';
 import tabsSelectComp from './components/tabs-select';
 import transTable from './components/trans-table';
-import signUp from './components/sign-up';
 import mask from './components/mask';
 import svg4everybody from './components/svg4everybody';
 import pageReload from './components/page-reload';
 import payTabs from './components/pay-tabs';
-import calcItogo from './components/calc-itogo';
 
 
 
@@ -25,7 +22,6 @@ $(function(){
 	gumburger();
 	popups();
 	validate();
-	subscribe();
 	accordion();
 	textTable();
 	fullscreenMenu();
@@ -34,12 +30,10 @@ $(function(){
 	tabsComp();
 	tabsSelectComp();
 	transTable();
-	signUp();
 	mask();
 	svg4everybody();
 	pageReload();
 	payTabs();
-	calcItogo();
 });
 
 
